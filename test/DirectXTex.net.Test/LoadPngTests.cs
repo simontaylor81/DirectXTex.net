2ad9362c64bc8e31269ca21ab70d640318d6b7f1
simontaylor81 <simontaylor81@hotmail.com>
2016-03-03 19:59:20 +0100

    Added tests for more file formats.

--- test/DirectXTex.net.Test/LoadPngTests.cs
+++ test/DirectXTex.net.Test/LoadPngTests.cs
@@ -12,16 +12,47 @@ namespace DirectXTexNet.Test
 {
 	public class LoadPngTests
 	{
-		[Test]
-		public void LoadPng()
+		// Test for WIC loading path, which we can compare to the result of loading using System.Drawing.
+		[TestCase("ThisIsATest.png")]
+		[TestCase("ThisIsATest.bmp")]
+		[TestCase("ThisIsATest.jpg")]
+		public void LoadWIC(string filename)
 		{
-			var path = GetImagePath("ThisIsATest.png");
+			var path = GetImagePath(filename);
 
-			var image = DirectXTex.LoadFromWICFile(GetImagePath("ThisIsATest.png"));
-			var bytes = image.GetRawBytes(0, 0);
+			// Load image with DirectXTex.
+			using (var image = DirectXTex.LoadFromWICFile(path))
+			{
+				var bytes = image.GetRawBytes(0, 0);
 
-			var expected = new Bitmap(path).GetRawBytesRGBA();
-			Assert.That(bytes, Is.EqualTo(expected));
+				// Load the same image with System.Drawing.
+				using (var expected = new Bitmap(path))
+				{
+					// Assert that they match.
+					Assert.That(bytes, Is.EqualTo(expected.GetRawBytesRGBA()));
+				}
+			}
+		}
+
+		// Test for WIC loading path, which we can compare to the result of loading using System.Drawing.
+		[Test]
+		public void LoadTga()
+		{
+			var path = GetImagePath("ThisIsATest.tga");
+			var refPath = GetImagePath("ThisIsATest.png");
+
+			// Load image with DirectXTex.
+			using (var image = DirectXTex.LoadFromTGAFile(path))
+			{
+				var bytes = image.GetRawBytes(0, 0);
+
+				// Can't load a tga with System.Drawing, so load an identical png.
+				using (var expected = new Bitmap(refPath))
+				{
+					// Assert that they match.
+					Assert.That(bytes, Is.EqualTo(expected.GetRawBytesRGBA()));
+				}
+			}
 		}
 
 		// Assembly is in bin/<config>/
