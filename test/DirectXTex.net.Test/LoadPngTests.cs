7a6746b07d19320ea9cdfdf53d232716e2364a62
simontaylor81 <simontaylor81@hotmail.com>
2016-03-03 19:50:07 +0100

    Added test project and a basic test

---
+++ test/DirectXTex.net.Test/LoadPngTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DirectXTex.net.Test.Util;
+using NUnit.Framework;
+
+namespace DirectXTexNet.Test
+{
+	public class LoadPngTests
+	{
+		[Test]
+		public void LoadPng()
+		{
+			var path = GetImagePath("ThisIsATest.png");
+
+			var image = DirectXTex.LoadFromWICFile(GetImagePath("ThisIsATest.png"));
+			var bytes = image.GetRawBytes(0, 0);
+
+			var expected = new Bitmap(path).GetRawBytesRGBA();
+			Assert.That(bytes, Is.EqualTo(expected));
+		}
+
+		// Assembly is in bin/<config>/
+		private string GetImagePath(string filename) =>
+			Path.Combine(
+				Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location),
+				"../../Images",
+				filename);
+	}
+}
