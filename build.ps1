107df9ad8115381cb8d1a97624692ddf7aa7ea1c
simontaylor81 <simontaylor81@hotmail.com>
2016-03-05 23:19:38 +0100

    Nicer logging in build script

--- build.ps1
+++ build.ps1
@@ -2,8 +2,6 @@
 $config = 'Release'
 $platforms = 'x86', 'x64'
 
-echo $psversiontable
-
 # Hook up AppVeyor logger if it exists.
 if (Test-Path 'C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll')
 {
@@ -12,6 +10,8 @@ if (Test-Path 'C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll')
 
 foreach ($platform in $platforms)
 {
+	echo "Building $config|$platform"
+
 	& msbuild 'DirectXTexNet.sln' "/property:Configuration=$config" "/property:Platform=$platform" '/verbosity:Minimal' '--%' $loggerParam
 	if (!$?)
 	{
