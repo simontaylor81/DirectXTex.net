7f5cb81558dafce687ce652743b66c05bf0f7693
simontaylor81 <simontaylor81@hotmail.com>
2016-03-05 22:17:12 +0100

    Fail build if msbuild returns error code

--- build.ps1
+++ build.ps1
@@ -3,12 +3,16 @@ $config = 'Release'
 $platforms = 'x86', 'x64'
 
 # Hook up AppVeyor logger if it exists.
-if (Test-Path "C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll")
-{
+#if (Test-Path "C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll")
+#{
 	$loggerParam = '/logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"'
-}
+#}
 
 foreach ($platform in $platforms)
 {
 	& msbuild 'DirectXTexNet.sln' "/property:Configuration=$config" "/property:Platform=$platform" '/verbosity:Minimal' $loggerParam
+	if (!$?)
+	{
+		throw "Build failed"
+	}
 }
