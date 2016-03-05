3b7de6d7545348d2dedc5dee8133323520063b6a
simontaylor81 <simontaylor81@hotmail.com>
2016-03-05 12:56:51 +0100

    Renamed main project to DirectXTexNetImpl

--- src/DirectXTexNetImpl/AssemblyInfo.cpp
+++ src/DirectXTexNetImpl/AssemblyInfo.cpp
@@ -11,7 +11,7 @@ using namespace System::Security::Permissions;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly:AssemblyTitleAttribute(L"DirectXTexNet")];
+[assembly:AssemblyTitleAttribute(L"DirectXTexNetImpl")];
 [assembly:AssemblyDescriptionAttribute(L"Minimal .NET wrapper around the DirectXTex library")];
 [assembly:AssemblyConfigurationAttribute(L"")];
 [assembly:AssemblyCompanyAttribute(L"Simon Taylor")];
