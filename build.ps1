75a36392331ddabff436fa2cee84989cd4dda93e
simontaylor81 <simontaylor81@hotmail.com>
2016-03-05 22:10:37 +0100

    Use custom build script
    to allow multiple platforms to be build before nuget pack

---
+++ build.ps1
@@ -0,0 +1,14 @@
+# Build platforms & configs required to build the nuget package.
+$config = 'Release'
+$platforms = 'x86', 'x64'
+
+# Hook up AppVeyor logger if it exists.
+if (Test-Path "C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll")
+{
+	$loggerParam = '/logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"'
+}
+
+foreach ($platform in $platforms)
+{
+	& msbuild 'DirectXTexNet.sln' "/property:Configuration=$config" "/property:Platform=$platform" '/verbosity:Minimal' $loggerParam
+}
