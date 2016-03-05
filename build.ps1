a53061b997cb89ceaef9bee1087a1dad93d6867b
simontaylor81 <simontaylor81@hotmail.com>
2016-03-05 22:27:51 +0100

    Build script fix attempt

--- build.ps1
+++ build.ps1
@@ -3,10 +3,10 @@ $config = 'Release'
 $platforms = 'x86', 'x64'
 
 # Hook up AppVeyor logger if it exists.
-#if (Test-Path "C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll")
-#{
-	$loggerParam = '/logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"'
-#}
+if (Test-Path 'C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll')
+{
+	$loggerParam = '/logger:\"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll\"'
+}
 
 foreach ($platform in $platforms)
 {
